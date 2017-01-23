97975be82e0c2c7c9dc1a7eef4bcb587faa1624d
bednayb <bednaybence@gmail.com>
2017-01-23 10:35:42 +0100

    add basic server

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-"use strict";
+'use strict';
 
-console.log("aniko");
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var validator = require('./numberify.js');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.post('/turnomatic', function (req, res) {
+  var options = req.body.case;
+
+
+
+
+
+app.listen(3000);
