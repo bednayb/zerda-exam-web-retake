5e221f96cb60f13d43fe4b5d751a740035ff5a2c
bednayb <bednaybence@gmail.com>
2017-01-23 10:22:09 +0100

    add listener to button

--- client/script.js
+++ client/script.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("aniko");
+var button = document.querySelector('button');
+
+button.addEventListener('click', function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:3000/turnomatic');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  var DataFromTheClient = {
+      "case": "hoodie"
+  };
+    xhr.send(JSON.stringify(DataFromTheClient));
+})
