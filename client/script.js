3a04ab643592e2c906132d3e4e42dbde9fd259f2
bednayb <bednaybence@gmail.com>
2017-01-23 10:30:30 +0100

    add variable to options

--- client/script.js
+++ client/script.js
@@ -1,13 +1,28 @@
 "use strict";
 
 var button = document.querySelector('button');
+var admin = document.querySelector('#AdminOptions');
+var tech = document.querySelector('#TechOptions');
+var hoodie = document.querySelector('#HoodieOptions');
+
+
 
 button.addEventListener('click', function () {
   var xhr = new XMLHttpRequest();
+  console.log(tech.value);
   xhr.open('POST', 'http://localhost:3000/turnomatic');
   xhr.setRequestHeader('Content-Type', 'application/json');
   var DataFromTheClient = {
       "case": "hoodie"
   };
     xhr.send(JSON.stringify(DataFromTheClient));
+
+
+// xhr.onreadystatechange = function () {
+//     if(xhr.readyState === XMLHttpRequest.DONE) {
+//       var li = document.createElement('li');
+//       li.innerText = xhr.responseText;
+//       ul.appendChild(li);
+//     }
+//   }
 })
