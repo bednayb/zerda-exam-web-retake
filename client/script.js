29fd2db922013e79636c6a20d2f04f1836b0ccad
bednayb <bednaybence@gmail.com>
2017-01-23 11:03:31 +0100

    add onreadystage function

--- client/script.js
+++ client/script.js
@@ -4,25 +4,24 @@ var button = document.querySelector('button');
 var admin = document.querySelector('#AdminOptions');
 var tech = document.querySelector('#TechOptions');
 var hoodie = document.querySelector('#HoodieOptions');
-
-
+var select = document.querySelector('#select');
+var ul = document.querySelector('#result');
 
 button.addEventListener('click', function () {
+  console.log(select.value);
   var xhr = new XMLHttpRequest();
-  console.log(tech.value);
   xhr.open('POST', 'http://localhost:3000/turnomatic');
   xhr.setRequestHeader('Content-Type', 'application/json');
   var DataFromTheClient = {
-      case: "hoodie"
+      "case":select.value
+  };
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      var li = document.createElement('li');
+      li.innerText = xhr.responseText;
+      ul.appendChild(li);
+    }
   };
     xhr.send(JSON.stringify(DataFromTheClient));
 
-
-// xhr.onreadystatechange = function () {
-//     if(xhr.readyState === XMLHttpRequest.DONE) {
-//       var li = document.createElement('li');
-//       li.innerText = xhr.responseText;
-//       ul.appendChild(li);
-//     }
-//   }
 })
